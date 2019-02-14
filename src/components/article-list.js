7b77ff52d35b6b296389c8621fd4c8378ae889d1
Roman Iakobchuk <kaboom.ua@gmail.com>
2019-02-14 19:36:55 +0200

    add accordion hook

--- src/components/article-list.js
+++ src/components/article-list.js
@@ -1,16 +1,29 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Article from './article'
+import useAccordion from '../custom-hooks/accordion'
+
+export default function ArticleList({ articles }) {
+  const { openArticleId, toggleOpenArticle } = useAccordion()
+
+  const articleItems = articles.map((article) => (
+    <li key={article.id}>
+      <Article
+        article={article}
+        onBtnClick={toggleOpenArticle(article.id)}
+        isOpen={article.id === openArticleId}
+      />
+    </li>
+  ))
+
+  return <ul>{articleItems}</ul>
+}
+
 //import AccodrionComponent from './accordion-component'
+/*
 import accordion from '../decorators/accordion'
 
-class ArticleList extends Component {
-  render() {
-    return <ul>{this.getArticles()}</ul>
-  }
-
-  getArticles() {
-    const { articles, toggleOpenArticle, openArticleId } = this.props
-    return articles.map((article) => (
+function ArticleList({ articles, toggleOpenArticle, openArticleId }) {
+    const articleItems = articles.map((article) => (
       <li key={article.id}>
         <Article
           article={article}
@@ -19,7 +32,9 @@ class ArticleList extends Component {
         />
       </li>
     ))
-  }
+
+    return <ul>{articleItems}</ul>
 }
 
 export default accordion(ArticleList)
+*/
