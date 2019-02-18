253c3bef1609cd2f9ba1aaabe2fb5373340237c2
Roman Iakobchuk <kaboom.ua@gmail.com>
2019-02-18 18:55:35 +0200

    add defaultProps

--- src/components/comment-list.js
+++ src/components/comment-list.js
@@ -10,6 +10,10 @@ class CommentList extends Component {
     toggleOpen: PropTypes.func
   }
 
+  static defaultProps = {
+    comments: []
+  }
+
   render() {
     const { isOpen, toggleOpen } = this.props
     const text = isOpen ? 'hide comments' : 'show comments'
